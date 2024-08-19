91e2b4a3040e4eacd9647268d40a39342602c373
Isaac Patricio Coste Burgos <isaac_coste@ucne.edu.do>
2024-08-19 03:45:09 -0400

    Cree el contexto y agrege el modelo de User

---
+++ UserContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserApp;
+
+public class UserContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("UserDatabase");
+    }
+}
